f8173075175cb02f7552042a0bb493f301d1b106
Tanzina Naznin Shaila <68160814+tnshaila@users.noreply.github.com>
2023-04-19 13:51:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,111 @@ let weather = {
       this.fetchWeather(document.querySelector(".search-bar").value);
     },
   };
+function covertTimeTo12HourFormat(time) {
+  let hour = time.split(":")[0];
+  let minute = time.split(":")[1];
+  let ampm = hour >= 12 ? "pm" : "am";
+  hour = hour % 12;
+  hour = hour ? hour : 12; // the hour '0' should be '12'
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  let strTime = hour + ":" + minute + " " + ampm;
+  return strTime;
+}
+
+// function to get day name from date
+function getDayName(date) {
+  let day = new Date(date);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day.getDay()];
+}
+
+// function to get uv index status
+function measureUvIndex(uvIndex) {
+  if (uvIndex <= 2) {
+    uvText.innerText = "Low";
+  } else if (uvIndex <= 5) {
+    uvText.innerText = "Moderate";
+  } else if (uvIndex <= 7) {
+    uvText.innerText = "High";
+  } else if (uvIndex <= 10) {
+    uvText.innerText = "Very High";
+  } else {
+    uvText.innerText = "Extreme";
+  }
+}
+
+// function to get humidity status
+function updateHumidityStatus(humidity) {
+  if (humidity <= 30) {
+    humidityStatus.innerText = "Low";
+  } else if (humidity <= 60) {
+    humidityStatus.innerText = "Moderate";
+  } else {
+    humidityStatus.innerText = "High";
+  }
+}
+
+// function to get visibility status
+function updateVisibiltyStatus(visibility) {
+  if (visibility <= 0.03) {
+    visibilityStatus.innerText = "Dense Fog";
+  } else if (visibility <= 0.16) {
+    visibilityStatus.innerText = "Moderate Fog";
+  } else if (visibility <= 0.35) {
+    visibilityStatus.innerText = "Light Fog";
+  } else if (visibility <= 1.13) {
+    visibilityStatus.innerText = "Very Light Fog";
+  } else if (visibility <= 2.16) {
+    visibilityStatus.innerText = "Light Mist";
+  } else if (visibility <= 5.4) {
+    visibilityStatus.innerText = "Very Light Mist";
+  } else if (visibility <= 10.8) {
+    visibilityStatus.innerText = "Clear Air";
+  } else {
+    visibilityStatus.innerText = "Very Clear Air";
+  }
+}
+
+// function to get air quality status
+function updateAirQualityStatus(airquality) {
+  if (airquality <= 50) {
+    airQualityStatus.innerText = "Good👌";
+  } else if (airquality <= 100) {
+    airQualityStatus.innerText = "Moderate😐";
+  } else if (airquality <= 150) {
+    airQualityStatus.innerText = "Unhealthy for Sensitive Groups😷";
+  } else if (airquality <= 200) {
+    airQualityStatus.innerText = "Unhealthy😷";
+  } else if (airquality <= 250) {
+    airQualityStatus.innerText = "Very Unhealthy😨";
+  } else {
+    airQualityStatus.innerText = "Hazardous😱";
+  }
+}
+
+// function to handle search form
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let location = search.value;
+  if (location) {
+    currentCity = location;
+    getWeatherData(location, currentUnit, hourlyorWeek);
+  }
+});
+
+// function to conver celcius to fahrenheit
+function celciusToFahrenheit(temp) {
+  return ((temp * 9) / 5 + 32).toFixed(1);
+}
   
   document.querySelector(".search button").addEventListener("click", function () {
     weather.search();
